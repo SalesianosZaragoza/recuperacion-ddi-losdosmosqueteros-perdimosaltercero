bf663edb54c41812a86e8ca5108bdfc2e727296b
Carlos <ventura.tocar22@zaragoza.salesianos.edu>
2024-02-05 09:18:08 +0100

    ola

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("F");
+		System.out.println("Hola");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
